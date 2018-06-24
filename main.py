f5e7afd092595bfa1a47ed4269990353f358bf6e
Mohit Jain <mohitjain1999@yahoo.com>
2018-06-25 01:16:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from perceptron import Perceptron
 import pdr
 
-def __main__:
+def main():
 
 	# get the iris data
 	df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
