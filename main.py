4b7db819f4b752ec51b4b6cc1b28640c62d61c01
Mohit Jain <mohitjain1999@yahoo.com>
2018-06-25 01:26:41 +0530

    Fix errors and add graphs.

--- main.py
+++ main.py
@@ -5,41 +5,40 @@ import matplotlib.pyplot as plt
 from perceptron import Perceptron
 import pdr
 
-def main():
 
-	# get the iris data
-	df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-		header = None)
+# get the iris data
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+	header = None)
 
-	# Plot 100 samples od the data
+# Plot 100 samples od the data
 
-	y = dif.iloc[0:100, 4].values
-	y = np.where(y == 'Iris-setosa', -1, 1)
-	X = dif.iloc[0:100, [0, 2]].values
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
 
-	plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
-	plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'x', label = 'versicolor')
-	plt.xlabel('petal length')
-	plt.ylabel('sepal length')
-	plt.legend(loc = 'upper left')
-	plt.show()
+plt.scatter(X[:50, 0], X[:50, 1], color = 'red', marker = 'o', label = 'setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'x', label = 'versicolor')
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc = 'upper left')
+plt.show()
 
-	# get the perceptron model
-	model = Perceptron(eta = 0.1, n_iter = 10)
+# get the perceptron model
+model = Perceptron(eta = 0.1, n_iter = 10)
 
-	# train the model
-	model.fit(X, y)
+# train the model
+model.fit(X, y)
 
-	# plot the training error
-	plt.plot(range(1, len(model.errors_) + 1), model.errors_, marker = 'o')
-	plt.xlabel('Epochs')
-	plt.ylabel('Number of misclassifications')
-	plt.show()
+# plot the training error
+plt.plot(range(1, len(model.errors_) + 1), model.errors_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.show()
 
-	# create decision regions
-	pdr.plot_decision_regions(X, y, classifier = model)
-	plt.xlabel('sepal length [cm]')
-	plt.ylabel('petal length [cm]')
-	plt.legend(loc = 'upper left')
-	plt.show()
+# create decision regions
+pdr.plot_decision_regions(X, y, classifier = model)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc = 'upper left')
+plt.show()
 
--- pdr.py
+++ pdr.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
 from matplotlib.colors import ListedColormap
 
 """ Function to color the deciosion regions """
@@ -14,8 +17,8 @@ def plot_decision_regions(X, y, classifier, resolution = 0.02):
 
 	x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 	x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-	xx1, xx2 = np.meshgrid(np.arrange(x1_min, x1_max, resolution),
-		np.arrange(x2_min, x2_max, resolution))
+	xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+		np.arange(x2_min, x2_max, resolution))
 
 	z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
 	Z = z.reshape(xx1.shape)
